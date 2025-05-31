f93e6b44bf1fbf289652fc6a88af89c92b780779
amelmochi <novaivtihal123@students.unnes.ac.id>
2025-05-31 21:28:18 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,21 @@
+# restaurant_dashboard.py
 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+# Load data
+df = pd.read_csv("semarang_resto_dataset.csv")
+
+# Sidebar navigation
+st.sidebar.title("📊 Restaurant Dashboard")
+page = st.sidebar.radio("Pilih Halaman:", [
+    "1. Background", 
+    "2. Data Visualization", 
+    "3. Clustering", 
+    "4. Restaurant by Cluster", 
+    "5. Profile"
+])
