3eca2d0de94f1e8eb4c05f57a5fd6cc8cba4ed95
amelmochi <novaivtihal123@students.unnes.ac.id>
2025-05-31 21:25:07 +0700

    Update 4_Restaurant By Cluster.py

--- Pages/4_Restaurant By Cluster.py
+++ Pages/4_Restaurant By Cluster.py
@@ -1,1 +1,8 @@
+elif page.startswith("4"):
+    st.title("🏷️ Restaurant by Cluster")
 
+    if 'cluster' in df.columns:
+        cluster_num = st.selectbox("Pilih cluster:", sorted(df['cluster'].unique()))
+        st.write(df[df['cluster'] == cluster_num])
+    else:
+        st.warning("Silakan lakukan clustering terlebih dahulu di halaman 'Clustering'.")
