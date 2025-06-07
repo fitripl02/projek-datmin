adb5bb61dd07a799fafe2891d3164660415d4e0c
kansha02 <khoneykayla@gmail.com>
2025-06-07 22:59:43 +0700

    Update and rename 1_Background.py to 1_eda.py

--- Pages/1_Background.py
+++
@@ -1,7 +0,0 @@
-if page.startswith("1"):
-    st.title("📚 Background")
-    st.write("""
-    Dashboard ini dibuat untuk menganalisis restoran di Semarang berdasarkan berbagai fitur seperti rating, harga, dan lokasi. 
-    Dengan menggunakan visualisasi data dan teknik clustering, kita dapat melihat pola dan rekomendasi yang muncul dari dataset restoran tersebut.
-    """)
-    st.dataframe(df.head())
